fad50bf5672136a6eb15583de95916159695169f
unknownhggc <gr5881567@gmail.com>
2025-07-04 18:39:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45c
 API_ID = int(getenv("API_ID", ""))
 API_HASH = str(getenv("API_HASH", ""))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "0") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 MONGO_DB = str(getenv("MONGO_DB", "",))
 DEF_CAP = str(
     getenv(
